3f081fd8031c222b758c9f9b5cd3cfb0fa43feae
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-09-25 19:14:32 -0400

    added review component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Review from "./components/Review";
+
 import Home from "./pages/Home";
 import Index from "./pages/Index";
 import New from "./pages/New";
 import Show from "./pages/Show";
 import Edit from "./pages/Edit";
-import Footer from "./components/Footer";
 import FourOFour from "./pages/FourOFour";
 
 function App() {
@@ -20,6 +22,7 @@ function App() {
           <Route path="/cards/new" element={<New />} />
           <Route path="/cards/:id" element={<Show />} />
           <Route path="/cards/:id/edit" element={<Edit />} />
+          <Route path="/reviews" element={ <Review /> } />
           <Route path="*" element={<FourOFour />} />
         </Routes>
         <Footer />
